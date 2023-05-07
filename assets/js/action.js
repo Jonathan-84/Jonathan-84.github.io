292b81f60486b85ea0c726f78d3f94a6b27c4665
Jonathan Robinson <72467031+Jonathan-84@users.noreply.github.com>
2023-05-07 09:53:30 -0400

    trying pwa idea

--- assets/js/action.js
+++ assets/js/action.js
@@ -1,2 +1,7 @@
 var myCarousel = document.querySelector('#myCarousel')
 var carousel = new bootstrap.Carousel(myCarousel)
+
+if ("serviceWorker" in navigator) {
+    // register service worker
+    navigator.serviceWorker.register("service-worker.js");
+  }
